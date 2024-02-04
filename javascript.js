ffcf39b7f0c19f2c2392db7edf26494916a4a202
unknown <liethdallal@gmail.com>
2024-02-04 14:08:55 -0600

    palindrome question more efficient method found and added for future refrence

--- javascript.js
+++ javascript.js
@@ -10,3 +10,23 @@
 // };
 
 // console.log( isPalindrome(233))
+
+//This provides a much better runtime but memory is not great 
+// var isPalindrome = function(x) {
+//     const str = x.toString();
+//     let left = 0;
+//     let right = str.length - 1;
+    
+//     while (left < right) {
+//       if (str[left] !== str[right]) {
+//         return false;
+//       }
+//       left++;
+//       right--;
+//     }
+    
+//     return true;
+//   };
+  
+
+// console.log(isPalindrome(10));
