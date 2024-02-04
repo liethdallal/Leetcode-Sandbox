3801508faed43121e1034e5540f00bad82b93c9d
unknown <liethdallal@gmail.com>
2024-02-04 13:49:57 -0600

    palindrome question solved will look up more efficient methods

---
+++ javascript.js
@@ -0,0 +1,12 @@
+// 9. Palindrome
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+// var isPalindrome = function(x) {
+//   if(x.toString() == x.toString().split('').reverse().join(''))  {
+//     return true
+//   }
+//   else{
+//     return false
+//   }
+// };
+
+// console.log( isPalindrome(233))
