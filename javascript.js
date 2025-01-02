ded86ba654bf7175a4a516a88b2e9c5a609e9e48
unknown <liethdallal@gmail.com>
2025-01-01 19:33:52 -0600

    cleaned up code ready to start working on new problems and ready for a big year of coding

--- javascript.js
+++ javascript.js
@@ -94,20 +94,20 @@
 // Write a function to find the longest common prefix string amongst an array of strings.
 
 // If there is no common prefix, return an empty string "".
-var longestCommonPrefix = function(strs) {
-    if (strs.length === 0) return "";
+// var longestCommonPrefix = function(strs) {
+//     if (strs.length === 0) return "";
 
-    let prefix = "";
+//     let prefix = "";
 
-    for (let i = 0; i < strs[0].length; i++) {
-        let char = strs[0][i];
+//     for (let i = 0; i < strs[0].length; i++) {
+//         let char = strs[0][i];
         
-        for (let j = 1; j < strs.length; j++) {
-            if (strs[j][i] !== char || i === strs[j].length) {
-                return prefix;
-            }
-        }
-        prefix += char;
-    }
-    return prefix;
-};
+//         for (let j = 1; j < strs.length; j++) {
+//             if (strs[j][i] !== char || i === strs[j].length) {
+//                 return prefix;
+//             }
+//         }
+//         prefix += char;
+//     }
+//     return prefix;
+// };
